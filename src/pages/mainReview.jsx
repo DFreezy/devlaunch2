a7c3fe671120a652743f6cf4e235be05665d593e
CodeSpace Academy <duwaynefrieslaar23@gmail.com>
2024-10-14 18:29:49 +0200

    added nav links
    Added nav links to review page

--- src/pages/mainReview.jsx
+++ src/pages/mainReview.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Review(){
     return(
         <>
-        
+        <nav>
+            <ul className="nav">
+                <Link to=""><li>Home</li></Link>
+                <Link to=""><li>Bootcamps</li></Link>
+                <Link to=""><li>Review</li></Link>
+                <Link to=""><li>Contact us</li></Link>
+            </ul>
+        </nav>
         </>
     )
 }
